8409363736c1dacd94514f7db35513cbc2868359
rezalimena <rezaliem07@gmail.com>
2020-06-06 11:57:50 -0700

    final

--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 var morgan = require('morgan');
+var routes = require('./routes');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
-var routes = require('./routes');
-routes(app);
-
+app.use('/user',routes);
 app.use('/auth',require('./middleware'));
 
 const port = process.env.port || 3000;
--- routes.js
+++ routes.js
@@ -1,13 +1,15 @@
 'use strict'
+const express = require('express');
+var app = express.Router();
+var myjson = require('./controller');
 
-module.exports = function(app) {
-    var myjson = require('./controller');
+app.route('/').get(myjson.index);
+app.route('/tampil').get(myjson.showData);
+app.route('/tampil/:id').get(myjson.showDataId); 
+app.route('/mahasiswa').post(myjson.addData); 
+app.route('/mahasiswa/:id').put(myjson.editData); 
+app.route('/mahasiswa/delete/:id').delete(myjson.deleteData); 
+app.route('/shownested').get(myjson.nestedJSON); 
 
-    app.route('/').get(myjson.index);
-    app.route('/tampil').get(myjson.showData);
-    app.route('/tampil/:id').get(myjson.showDataId); 
-    app.route('/mahasiswa').post(myjson.addData); 
-    app.route('/mahasiswa/:id').put(myjson.editData); 
-    app.route('/mahasiswa/delete/:id').delete(myjson.deleteData); 
-    app.route('/shownested').get(myjson.nestedJSON); 
-}
+
+module.exports = app;
